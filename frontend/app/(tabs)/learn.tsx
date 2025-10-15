8017138c78abb04d04491a01e232c8d9bd49580f
Melyn Were <WereMA@cardiff.ac.uk>
2025-10-15 11:58:46 +0100

    create stylesheet for styling

--- frontend/app/(tabs)/learn.tsx
+++ frontend/app/(tabs)/learn.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "#ECE6F0",
+        borderRadius: 20,
+        width: "90%",
+        margin: 24,
+        padding: 24,
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        shadowRadius: 3.84,
 
+    },
+    cardHeading: {
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 12
+    },
+    cardSummary: {
+        textAlign: "justify",
+        fontSize: 15,
+    }
+
+ });
 export default function LearnScreen() {
     return (
         <ScrollView>
-            <View style={{
-                backgroundColor: "#ECE6F0",
-                borderRadius: 20,
-                width: "90%",
-                margin: 24,
-                padding: 24,
-                shadowColor: "#000",
-                shadowOffset: {width: 0, height: 4},
-                shadowOpacity: 0.3,
-                shadowRadius: 3.84,
-
-            }}
-            >
-            <Text style={{
-                fontSize:22,
-                fontWeight: "bold",
-                marginBottom: 12
-            }}>Surviving Menopause</Text>
-            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            <View style={styles.card}>            
+            <Text style={styles.cardHeading}>Surviving Menopause</Text>
+            <Text style={styles.cardSummary}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Nulla vehicula vel augue ac pellentesque. 
                 Mauris odio neque, viverra in ante eu, vestibulum scelerisque quam. 
                 Vestibulum pulvinar nunc libero. Aenean vestibulum.</Text>
