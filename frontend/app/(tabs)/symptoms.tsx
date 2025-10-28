4d3ab6182d064de29e1637f478f49f1a64169205
Melyn Were <WereMA@cardiff.ac.uk>
2025-10-28 12:52:47 +0000

    style symptoms screen

--- frontend/app/(tabs)/symptoms.tsx
+++ frontend/app/(tabs)/symptoms.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
     },
     symptomsMenu: {
-        marginBottom:10,
+        marginBottom:15,
         borderRadius:10,
         margin:15,
         padding: 10,   
@@ -32,12 +32,18 @@ export default function SymptomsScreen() {
 
     return (
         <ScrollView>
-            <Text>Hi there!</Text>
-            <Text>Today is: {today}</Text>
 
+            <View style={{marginLeft: 15, marginTop:15, marginBottom:10}}>
+            <Text style={{fontSize: 40, fontWeight: 'bold', marginBottom: 10}}>Hi there!</Text>
+            <Text style={{fontWeight:'bold', fontSize: 30}}>Today: {today}</Text>
+            </View>
+
+            <View style={{alignItems:'center', marginTop:20, marginBottom:20}}>
+                <View style={{borderRadius: 200, height: 200, width: 200, backgroundColor:'orange'}}></View>
             <TouchableOpacity style={styles.button} onPress={() => console.log("Button pressed")}><Text style={{fontWeight:'bold'}}>LOG SYMPTOMS</Text></TouchableOpacity>
+            </View>
 
-            <View>
+            <View style={{marginTop:20}}>
             <TouchableOpacity style={styles.symptomsMenu} onPress={() => console.log("Button pressed")}>
                 <Text style={{fontWeight:'bold', fontSize: 20}}>View Past Entries</Text></TouchableOpacity>
             <TouchableOpacity style={styles.symptomsMenu} onPress={() => console.log("Button pressed")}>
